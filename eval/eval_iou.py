35d0b09076dd4c73dc14a00bf293844b32482808
Jacopo Spaccatrosi <jacopo.spaccatrosi@snapiot.com>
2024-02-12 20:13:49 +0100

    mod

--- eval/eval_iou.py
+++ eval/eval_iou.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 import os
 import importlib
 import time
-import utils.utils as myutils
+
 from PIL import Image
 from argparse import ArgumentParser
 
@@ -19,9 +19,10 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, CenterCrop, Normalize, Resize
 from torchvision.transforms import ToTensor, ToPILImage
 
+import utils as myutils
 from dataset import cityscapes
-from erfnet import ERFNet
-from train.erfnet import Net
+# from erfnet import ERFNet
+from erfnet import Net
 from transform import Relabel, ToLabel, Colorize
 from iouEval import iouEval, getColorEntry
 
@@ -57,23 +58,23 @@ input_transform_cityscapes = Compose([
 target_transform_cityscapes = Compose([
     Resize(512, Image.NEAREST),
     ToLabel(),
-    Relabel(255, 19),   #ignore label to 19
+    Relabel(255, 19),  # ignore label to 19
 ])
 
-def main(args):
 
+def main(args):
     print(f"Evaluation of mIoU using the metrics : {args.typeConfidence}")
 
     modelpath = args.loadDir + args.loadModel
     weightspath = args.loadDir + args.loadWeights
 
-    print ("Loading model: " + modelpath)
-    print ("Loading weights: " + weightspath)
+    print("Loading model: " + modelpath)
+    print("Loading weights: " + weightspath)
 
-    model = ERFNet(NUM_CLASSES)
+    model = Net(NUM_CLASSES)
     model2 = Net(NUM_CLASSES)
 
-    #model = torch.nn.DataParallel(model)
+    # model = torch.nn.DataParallel(model)
     if (not args.cpu):
         model = torch.nn.DataParallel(model).cuda()
 
@@ -121,7 +122,9 @@ def main(args):
     # map_location è un argomento di torch.load che specifica come e dove i tensori salvati devono essere mappati in memoria. Può essere utilizzato per forzare tutti i tensori ad essere caricati su CPU o su una specifica GPU, o per mapparli da una configurazione di hardware a un'altra.
     # Nel tuo esempio, map_location=lambda storage, loc: storage è una funzione lambda che ignora il loc (la localizzazione originale del tensore quando è stato salvato) e restituisce storage. Questo significa che i tensori saranno caricati sulla stessa tipologia di dispositivo da cui sono stati salvati (CPU o GPU).
     model = load_my_state_dict(model, torch.load(weightspath, map_location=lambda storage, loc: storage))
-    model2 = load_my_state_dict(model2, torch.load( args.loadDir + args.loadWeightsPruned, map_location=lambda storage, loc: storage))
+    model2 = load_my_state_dict(model2, torch.load(args.loadDir + args.loadWeightsPruned,
+                                                   map_location=lambda storage, loc: storage))
+
     argsPlus = {
         "listLayerPruning": ["non_bottleneck_1d"],
         "listNumLayerPruning": [],
@@ -131,32 +134,36 @@ def main(args):
     }
     argsPlus = SimpleNamespace(**argsPlus)
     model2 = myutils.remove_prunned_channels_from_model(model2, argsPlus)
-    print ("Model and weights LOADED successfully")
+    print("Model and weights LOADED successfully")
 
-    calibrationQuantization = DataLoader( cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset), num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
+    # calibrationQuantization = DataLoader( cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset), num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
 
     # commando per forzare il modello ad essere in modelità valutazione
     model.eval()
     model2.eval()
-    model2.quantize()
+    model2 = model2.to("cuda")
+    # model2.quantize()
+    # model2 = model2.to("cuda")
 
-    for step, (images, labels, _, _) in enumerate(calibrationQuantization):
-        if (not args.cpu):
-            images = images.cuda()
-            labels = labels.cuda()
+    # for step, (images, labels, _, _) in enumerate(calibrationQuantization):
+    #     if (not args.cpu):
+    #         images = images.cuda()
+    #         labels = labels.cuda()
 
-        model2.quantize_forward(images)
+    #     model2.quantize_forward(images)
 
-    model2.freeze()
+    # model2.freeze()
     print("Model Pruned and Quantized ... ")
 
-    if(not os.path.exists(args.datadir)):
-        print ("Error: datadir could not be loaded")
+    if (not os.path.exists(args.datadir)):
+        print("Error: datadir could not be loaded")
 
-    loader = DataLoader(cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset), num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
+    loader = DataLoader(
+        cityscapes(args.datadir, input_transform_cityscapes, target_transform_cityscapes, subset=args.subset),
+        num_workers=args.num_workers, batch_size=args.batch_size, shuffle=False)
 
     iouEvalVal = iouEval(NUM_CLASSES)
-    iouEvalVal2 = iouEvalVal(NUM_CHANNELS)
+    iouEvalVal2 = iouEval(NUM_CLASSES)
 
     start = time.time()
 
@@ -168,7 +175,8 @@ def main(args):
         inputs = Variable(images)
         with torch.no_grad():
             outputs = model(inputs)
-            outputs2 = model2.quantize_inference(images)
+            outputs2 = model2(inputs)
+            # outputs2 = model2.quantize_inference(inputs)
 
         finalOutput = outputs.max(1)[1].unsqueeze(1)
 
@@ -181,26 +189,26 @@ def main(args):
         if args.typeConfidence.casefold().replace(" ", "") == "maxentropy":
             eps = 1e-10
             probs = torch.nn.functional.softmax(outputs, dim=1)
-            entropy = torch.div(torch.sum(-probs * torch.log(probs + eps), dim=1),torch.log(torch.tensor(probs.shape[1]) + eps))
+            entropy = torch.div(torch.sum(-probs * torch.log(probs + eps), dim=1),
+                                torch.log(torch.tensor(probs.shape[1]) + eps))
             confidence = 1 - entropy
             weighted_output = probs * confidence.unsqueeze(1)
             _, predicted_classes = torch.max(weighted_output, dim=1)
             finalOutput = predicted_classes.unsqueeze(1)
 
         iouEvalVal.addBatch(finalOutput.data, labels)
-        iouEvalVal2.addBatch(outputs2.max(1)[1].unsqueeze(1))
+        iouEvalVal2.addBatch(outputs2.max(1)[1].unsqueeze(1).data, labels)
 
         filenameSave = filename[0].split("leftImg8bit/")[1]
 
-        #print (step, filenameSave)
-
+        # print (step, filenameSave)
 
     iouVal, iou_classes = iouEvalVal.getIoU()
     iouVal2, iou_classes2 = iouEvalVal2.getIoU()
 
     iou_classes_str = []
     for i in range(iou_classes.size(0)):
-        iouStr = getColorEntry(iou_classes[i])+'{:0.2f}'.format(iou_classes[i]*100) + '\033[0m'
+        iouStr = getColorEntry(iou_classes[i]) + '{:0.2f}'.format(iou_classes[i] * 100) + '\033[0m'
         iou_classes_str.append(iouStr)
 
     iou_classes_str2 = []
@@ -209,9 +217,9 @@ def main(args):
         iou_classes_str2.append(iouStr)
 
     print("---------------------------------------")
-    print("Took ", time.time()-start, "seconds")
+    print("Took ", time.time() - start, "seconds")
     print("=======================================")
-    #print("TOTAL IOU: ", iou * 100, "%")
+    # print("TOTAL IOU: ", iou * 100, "%")
     print("Per-Class IoU:")
     print(iou_classes_str[0], "Road")
     print(iou_classes_str[1], "sidewalk")
@@ -233,8 +241,8 @@ def main(args):
     print(iou_classes_str[17], "motorcycle")
     print(iou_classes_str[18], "bicycle")
     print("=======================================")
-    iouStr = getColorEntry(iouVal)+'{:0.2f}'.format(iouVal*100) + '\033[0m'
-    print ("MEAN IoU: ", iouStr, "%")
+    iouStr = getColorEntry(iouVal) + '{:0.2f}'.format(iouVal * 100) + '\033[0m'
+    print("MEAN IoU: ", iouStr, "%")
 
     print("---------------------------------------")
     print("Took ", time.time() - start, "seconds")
@@ -264,24 +272,22 @@ def main(args):
     iouStr = getColorEntry(iouVal2) + '{:0.2f}'.format(iouVal2 * 100) + '\033[0m'
     print("MEAN IoU: ", iouStr, "%")
 
+
 if __name__ == '__main__':
     parser = ArgumentParser()
 
     parser.add_argument('--state')
 
-    parser.add_argument('--loadDir',default="../trained_models/")
+    parser.add_argument('--loadDir', default="../trained_models/")
     parser.add_argument('--loadWeights', default="erfnet_pretrained.pth")
     parser.add_argument('--loadWeightsPruned', default="")
     parser.add_argument('--loadModel', default="erfnet.py")
-    parser.add_argument('--subset', default="val")  #can be val or train (must have labels)
+    parser.add_argument('--subset', default="val")  # can be val or train (must have labels)
     parser.add_argument('--datadir', default="/home/shyam/ViT-Adapter/segmentation/data/cityscapes/")
     parser.add_argument('--num-workers', type=int, default=torch.cuda.device_count())
     parser.add_argument('--batch-size', type=int, default=1)
     parser.add_argument('--cpu', action='store_true')
     parser.add_argument('--typeConfidence', default='MaxLogit')
-    parser.add_argument("--temperature", default = 1.0)
-
-    if os.path.basename(os.getcwd()) != "eval":
-        os.chdir("./eval")
+    parser.add_argument("--temperature", default=1.0)
 
     main(parser.parse_args())
---
+++ eval/utils.py
@@ -0,0 +1,192 @@
+import contextlib
+import copy
+import sys
+
+import torch.nn as nn
+import thop
+import torch
+import torch.nn.utils.prune as prune
+import logging
+import os
+import re
+from erfnet import non_bottleneck_1d,DownsamplerBlock
+
+
+def convert_model_from_dataparallel(model):
+
+    if isinstance(model, nn.DataParallel):
+        model = model.module
+
+    return model
+
+@contextlib.contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+def compute_difference_flop(modelOriginal,modelPruning):
+
+    logger = logging.getLogger('thop')
+    logger.setLevel(logging.WARNING)
+
+    input = torch.randn(1, 3, 512, 1024).to("cuda" if torch.cuda.is_available() else "cpu")
+    with suppress_stdout():
+        flopsOriginal, paramsOriginal = thop.profile(modelOriginal, inputs=(input,))
+        flopsPruning, paramsPrunning = thop.profile(modelPruning, inputs=(input,))
+    print(f"FLOPs modelOriginal : {flopsOriginal} - FLOPs modelPruning : {flopsPruning} the difference is : {flopsOriginal-flopsPruning}")
+    print(f"Params modelOriginal : {paramsOriginal} - Params modelPruning : {paramsPrunning} the difference is : {paramsOriginal - paramsPrunning}\n")
+
+    return flopsOriginal,flopsPruning,paramsOriginal,paramsPrunning
+
+def remove_mask_from_model_with_pruning(model,args):
+    if "encoder" in args.moduleErfnetPruning:
+        if args.typePruning.casefold().replace(" ", "") == "unstructured":
+            if isinstance(model, torch.nn.DataParallel):
+                for name, module in model.module.encoder.named_modules():
+                    if isinstance(module, non_bottleneck_1d) and "non_bottleneck_1d" in args.listLayerPruning:
+                        match = re.search(r'\d+', name)
+                        if len(args.listNumLayerPruning) == 0 or (match and str(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(layer, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(layer, name='bais')
+                    if isinstance(module, DownsamplerBlock) and "DownsamplerBlock" in args.listLayerPruning:
+                        if len(args.listNumLayerPruning) == 0 or (match and str(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(layer, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(layer, name='bais')
+            else:
+                for name, module in model.encoder.named_modules():
+                    if isinstance(module, non_bottleneck_1d) and "non_bottleneck_1d" in args.listLayerPruning:
+                        match = re.search(r'\d+', name)
+                        if len(args.listNumLayerPruning) == 0 or ( match and str(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(layer, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(layer, name='bais')
+                    if isinstance(module, DownsamplerBlock) and "DownsamplerBlock" in args.listLayerPruning:
+                        if len(args.listNumLayerPruning) == 0 or ( match and str(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(layer, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(layer, name='bais')
+        elif args.typePruning.casefold().replace(" ", "") == "structured":
+            if isinstance(model, torch.nn.DataParallel):
+                for name, module in model.module.named_modules():
+                    if isinstance(module, non_bottleneck_1d) and "non_bottleneck_1d" in args.listLayerPruning:
+                        match = re.search(r'\d+', name)
+                        if len(args.listNumLayerPruning) == 0 or ( match and int(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(layer, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(layer, name='bais')
+                    if isinstance(module, DownsamplerBlock) and "DownsamplerBlock" in args.listLayerPruning:
+                        match = re.search(r'\d+', name)
+                        if len(args.listNumLayerPruning) == 0 or (match and int(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(layer, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(layer, name='bias')
+            else:
+                for name, module in model.named_modules():
+                    if isinstance(module, non_bottleneck_1d) and "non_bottleneck_1d" in args.listLayerPruning:
+                        match = re.search(r'\d+', name)
+                        if len(args.listNumLayerPruning) == 0 or (match and int(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(module, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(module, name='bias')
+                    if isinstance(module, DownsamplerBlock) and "DownsamplerBlock" in args.listLayerPruning:
+                        match = re.search(r'\d+', name)
+                        if len(args.listNumLayerPruning) == 0 or ( match and int(match.group()) in args.listNumLayerPruning):
+                            for nameLayer, layer in [(name2, module2) for name2, module2 in module.named_modules() if any(substring in name2 for substring in args.listInnerLayerPruning)]:
+                                prune.remove(module, name='weight')
+                                if hasattr(layer, 'bias') and layer.bias is not None:
+                                    prune.remove(module, name='bias')
+
+    return model
+
+def remove_prunned_channels_from_model(modelOriginal, args):
+    modelOriginal = convert_model_from_dataparallel(modelOriginal)
+    modelFinal = copy.deepcopy(modelOriginal)
+    new_input_channel_next_layer = 0
+    parent_module = None
+    for nameLayer, layer in  modelOriginal.named_modules():
+        if isinstance(layer, nn.Conv2d) and not 'output_conv' in nameLayer:
+            if new_input_channel_next_layer > 0:
+                in_channels = new_input_channel_next_layer
+                input_changed = True
+            else:
+                in_channels = layer.in_channels
+                input_changed = False
+                non_zero_filters_prev_layer = None
+
+            # Trova i filtri non azzerati (esempio ipotetico, la logica esatta può variare)
+            non_zero_filters = layer.weight.data.sum(dim=(1, 2, 3)) != 0
+            new_out_channels = non_zero_filters.long().sum().item()
+            new_input_channel_next_layer = new_out_channels if new_out_channels != layer.out_channels else 0
+            if new_out_channels != layer.out_channels or in_channels != layer.in_channels:
+
+                if input_changed == False and layer.in_channels != new_out_channels:
+                    new_layer_adapt_input = nn.Conv2d(layer.in_channels, new_out_channels, kernel_size=1, stride=1)
+                    path_keys = str(nameLayer).split(".")
+                    parent_module = modelFinal
+                    for key in path_keys[0:-1]:  # Vai fino al genitore del layer
+                        parent_module = getattr(parent_module, key)
+
+                    setattr(parent_module, "adaptingInput", new_layer_adapt_input)
+
+                new_layer =  nn.Conv2d(in_channels=in_channels, out_channels=new_out_channels,
+                      kernel_size=layer.kernel_size, stride=layer.stride, padding=layer.padding,
+                      dilation=layer.dilation,bias=layer.bias is not None)
+
+                if input_changed:
+                    new_layer.weight.data = layer.weight.data[non_zero_filters][:,non_zero_filters_prev_layer,:,:]
+                    if layer.bias is not None:
+                        new_layer.bias.data = layer.bias.data[non_zero_filters]
+                else:
+                    new_layer.weight.data = layer.weight.data[non_zero_filters]
+                    if layer.bias is not None:
+                        new_layer.bias.data = layer.bias.data[non_zero_filters]
+
+
+                path_keys = str(nameLayer).split(".")
+                parent_module = modelFinal
+                for key in path_keys[0:-1]:  # Vai fino al genitore del layer
+                    parent_module = getattr(parent_module, key)
+
+                setattr(parent_module,path_keys[-1],new_layer)
+                non_zero_filters_prev_layer = non_zero_filters
+
+        if parent_module is not None and  isinstance(parent_module,DownsamplerBlock) and isinstance(layer, nn.Conv2d) and new_input_channel_next_layer == 0 :
+            #new_input_channel_next_layer = new_layer.in_channels+new_layer.out_channels
+            new_layer_adapt_input = nn.Conv2d(in_channels=new_layer.in_channels, out_channels=new_layer.out_channels, kernel_size=1, stride=1)
+            setattr(parent_module, "adaptingInput", new_layer_adapt_input)
+
+        if isinstance(layer, nn.BatchNorm2d) and  new_input_channel_next_layer > 0:
+            new_bn = nn.BatchNorm2d(new_input_channel_next_layer, eps=layer.eps)
+            path_keys = str(nameLayer).split(".")
+            parent_module = modelFinal
+            for key in path_keys[0:-1]:  # Vai fino al genitore del layer
+                parent_module = getattr(parent_module, key)
+            setattr(parent_module,path_keys[-1],new_bn)
+
+        if isinstance(layer,nn.ConvTranspose2d) and new_input_channel_next_layer >0 :
+            new_layer = nn.ConvTranspose2d(in_channels=new_input_channel_next_layer, out_channels=layer.out_channels,
+                                  kernel_size=layer.kernel_size, stride=layer.stride, padding=layer.padding,
+                                  output_padding = layer.output_padding,
+                                  dilation=layer.dilation, bias=layer.bias is not None)
+            path_keys = str(nameLayer).split(".")
+            parent_module = modelFinal
+            for key in path_keys[0:-1]:  # Vai fino al genitore del layer
+                parent_module = getattr(parent_module, key)
+            setattr(parent_module, path_keys[-1], new_layer)
+            new_input_channel_next_layer = 0
+
+
+    return modelFinal
